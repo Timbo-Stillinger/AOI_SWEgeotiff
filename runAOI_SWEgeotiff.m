66f121ea45b8ed69363290689299a19e4f02602c
Timbo <timbostillinger@gmail.com>
2021-04-08 11:59:14 -0700

    Update runAOI_SWEgeotiff.m

--- runAOI_SWEgeotiff.m
+++ runAOI_SWEgeotiff.m
@@ -5,16 +5,16 @@ AMSRsweDir='/path/to/folder/with/AMSR_SWE_Unified datasets';
 era5file='/path/to/folder/with/ERA5/langtang_snow_depth_daily.grib';
 aoiShp='/path/to/folder/with/AOI/langtang_catchment_nepal_wgs84.shp';
 outFile='langtang_catchment_nepal';
-makeAOI_SWEgeotiff(era5file,AMSRsweDir,aoiShp,outFolder,outFile)
+AOI_SWEgeotiff(era5file,AMSRsweDir,aoiShp,outFolder,outFile)
 
 %panjsher
 era5file='/path/to/folder/with/ERA5/panjsher_snow_depth_daily.grib';
 aoiShp='/path/to/folder/with/AOI/panjsher_basin_afghanistan_wgs84.shp';
 outFile='panjsher_basin_afghanistan';
-makeAOI_SWEgeotiff(era5file,AMSRsweDir,aoiShp,outFolder,outFile)
+AOI_SWEgeotiff(era5file,AMSRsweDir,aoiShp,outFolder,outFile)
 
 %wangchu
 era5file='/path/to/folder/with/ERA5/wangchu_snow_depth_daily.grib';
 aoiShp='/path/to/folder/with/AOI/wangchu_watershed_bhutan_wgs84.shp';
 outFile='wangchu_watershed_bhutan';
-makeAOI_SWEgeotiff(era5file,AMSRsweDir,aoiShp,outFolder,outFile)
+AOI_SWEgeotiff(era5file,AMSRsweDir,aoiShp,outFolder,outFile)
